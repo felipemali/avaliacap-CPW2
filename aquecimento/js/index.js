637dd3572995002dadeab10109dbc9b513fbf38f
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-02-16 22:34:14 -0400

    adicionando exemplo01

--- aquecimento/js/index.js
+++ aquecimento/js/index.js
@@ -13,6 +13,7 @@ const addPersonButton = document.createElement('button')
 addPersonButton.innerText = 'Adicionar'
 addPersonButton.className = 'person-button'
 addPersonButton.type = 'button'
+// <button type="button">Clique aqui</button>
 container.appendChild(addPersonButton)
 
 const clearListButton = document.createElement('button')
@@ -26,16 +27,15 @@ container.appendChild(peopleList)
 
 // Cria uma arrow function
 const addPerson = () => {
-    const name = faker.name.findName()
-    const personItem = document.createElement('li')
-    personItem.innerText = name
-    peopleList.appendChild(personItem)
+  const name = faker.name.findName()
+  const personItem = document.createElement('li')
+  personItem.innerText = name
+  peopleList.appendChild(personItem)
 }
 addPersonButton.onclick = addPerson
 
-
 const clearList = () => {
-    peopleList.innerHTML = ''
+  peopleList.innerHTML = ''
 }
 clearListButton.onclick = clearList
 //const firstPerson = document.querySelector('ul > li:nth-child(1)')
